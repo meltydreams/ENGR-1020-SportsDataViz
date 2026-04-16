e5169bfdce2ddde9cb5c8e1dcf3d0c631862d42d
Drew Tedesco <tboys109@gmail.com>
2026-04-16 16:28:54 -0400

    rotated api key again because vscode fucked up merge commit

--- js/firebase.js
+++ js/firebase.js
@@ -1,34 +1,16 @@
-<<<<<<< HEAD
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAb--hrH59doyVwnCdH2m4UNi3BEpPsnQ0",
-  authDomain: "engr-1020-sportsdataviz.firebaseapp.com",
-  projectId: "engr-1020-sportsdataviz",
-  storageBucket: "engr-1020-sportsdataviz.firebasestorage.app",
-  messagingSenderId: "26705667909",
-  appId: "1:26705667909:web:59a2fc47a1573339a9802c",
-  measurementId: "G-H4ZB8KKXHC"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 
-=======
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAb--hrH59doyVwnCdH2m4UNi3BEpPsnQ0",
-  authDomain: "engr-1020-sportsdataviz.firebaseapp.com",
-  projectId: "engr-1020-sportsdataviz",
-  storageBucket: "engr-1020-sportsdataviz.firebasestorage.app",
-  messagingSenderId: "26705667909",
-  appId: "1:26705667909:web:59a2fc47a1573339a9802c",
-  measurementId: "G-H4ZB8KKXHC"
-};
-
-const app = initializeApp(firebaseConfig);
-
->>>>>>> 9bb11ac8fda95ce289f4e57a08f4e22bc7672ed1
 export const db = getFirestore(app);
